e5028fca0a05acbe5f79fdeafd8ce2d979eeb1db
wish-i-was <forshad2016@yandex.ru>
2019-10-10 16:42:13 +0300

    extension mode, scope mode

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-Callback_url = "http://example.com/xss.js"
+Callback_url = "https://example.com"
 Payloads = "./payloads.txt"
 Headers = "./headers.txt"
 Parameters = "./parameters.txt"
+Extensions = ".webm, .ttf, .svg, .gif, .png, .js, .woff, .woff2, .jpeg, .jpg, .css, .ico, .m3u8, .ts, .bmp"
